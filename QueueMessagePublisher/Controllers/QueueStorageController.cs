cbdd375181978cba3cdd36c595842aee524a53be
majhaj <maja.hajda@gmail.com>
2023-07-04 12:48:35 +0200

    Adding example account details to connection strings

--- QueueMessagePublisher/Controllers/QueueStorageController.cs
+++ QueueMessagePublisher/Controllers/QueueStorageController.cs
@@ -12,7 +12,7 @@ namespace QueueMessagePublisher.Controllers
         [HttpPost("publish")]
         public async Task<IActionResult> Publish(ReturnDto returnDto)
         {
-            var connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+            var connectionString = "AccountName=account1;AccountKey=Eby8vdM02xNOUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
             var queueName = "returns";
 
             QueueClient queueClient = new QueueClient(connectionString, queueName);
